29ce73c4e788666c9fe88116fbe35a8405b42561
Petar Pejovic <petarpejovic1997@gmail.com>
2019-04-21 00:03:10 +0200

    Napravljen API za dohvatanje svih zadaca, ali samo id i naziva

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'))
 
-db.sequelize.sync({force:true}).then(() => { //force:true je da se nas dio baze uvijek iznova kreira
+db.sequelize.sync({force:false}).then(() => { //force:true je da se nas dio baze uvijek iznova kreira
     console.log("Usao u bazu!");
 }).catch((e) => {
     console.log("greska");
@@ -30,35 +30,47 @@ app.post('/addZadaca', function(req, res) {
         ukupniOstvareniBodovi : 0,
         postavka: bodyReq.postavka,
     }}).then(function(dodanaZadaca){
-            console.log(dodanaZadaca);
-            var idDodaneZadace = dodanaZadaca[0].idZadaca;
-            for(let i = 0; i < dodanaZadaca[0].brojZadataka; i++) {
-                db.Zadatak.findOrCreate({where :{
-                    idZadaca : idDodaneZadace,
-                    redniBrojZadatkaUZadaci : i,
-                    maxBrojBodova : bodyReq.listaBodova[i],
-                    brojOstvarenihBodova : 0,
-                    profesorovKomentar : "",
-                    datumPredaje : null,
-                    statusZadatka : "neposlan",
-                    sadrzajFile : null,
-                    velicinaFile : null,
-                    mimeTipUpdateZadatka : null             
-                }}).then(function(dodaniZadatak){
-                    console.log(dodaniZadatak);
-                    var idDodanogZadatka = dodaniZadatak[0].idZadatak;
-                    var mimeTipovi = [".pdf", ".zip", ".m", ".doc", ".txt"];
-                    for(let j = 0; j < 5; j++) {    
-                        if(bodyReq.listaTipova[i][j] === true) {
-                            db.MimeTip.findOrCreate({where: {
-                                idZadatak : idDodanogZadatka,
-                                mimeTip : mimeTipovi[j]
-                            }})
-                        }    
+        console.log(dodanaZadaca);
+        var idDodaneZadace = dodanaZadaca[0].idZadaca;
+        for(let i = 0; i < dodanaZadaca[0].brojZadataka; i++) {
+            db.Zadatak.findOrCreate({where :{
+                idZadaca : idDodaneZadace,
+                redniBrojZadatkaUZadaci : i,
+                maxBrojBodova : bodyReq.listaBodova[i],
+                brojOstvarenihBodova : 0,
+                profesorovKomentar : "",
+                datumPredaje : null,
+                statusZadatka : "neposlan",
+                sadrzajFile : null,
+                velicinaFile : null,
+                mimeTipUpdateZadatka : null             
+            }}).then(function(dodaniZadatak){
+                console.log(dodaniZadatak);
+                var idDodanogZadatka = dodaniZadatak[0].idZadatak;
+                var mimeTipovi = [".pdf", ".zip", ".m", ".doc", ".txt"];
+                for(let j = 0; j < 5; j++) {    
+                    if(bodyReq.listaTipova[i][j] === true) {
+                        db.MimeTip.findOrCreate({where: {
+                            idZadatak : idDodanogZadatka,
+                            mimeTip : mimeTipovi[j]
+                        }})
                     }    
-                })
-            }
-        });
+                }    
+            })
+        }
+    });
+});
+
+app.get('/getZadace', function(req, res) {
+    var nizZadaca = [];
+    
+    db.Zadaca.findAll().then(function(zadace){
+        for(let i = 0; i < zadace.length; i++) {
+            nizZadaca.push({id : zadace[i].idZadaca, naziv : zadace[i].naziv});
+        }
+        res.type("json");
+        res.end(JSON.stringify(nizZadaca))
+    });
 });
 
 app.listen(6001);
